358218830b1b1c03b6651e47dd28a9bf7ab9d6ac
jtfogarty <jeff@fogarty.org>
2026-01-08 17:13:04 -0600

    big fix

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+      "mason-org/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     }, 
 
@@ -31,7 +31,7 @@ return {
             "lua_ls",
             "graphql",
             "gopls",
-            "bufls",
+            "buf_ls",
             "emmet_ls",
             "prismals",
             "pyright",
